70998dee623914f335345aa6b587aa6758a5450f
TheAG2009 <adityaghodke2009@gmail.com>
2026-05-05 15:28:43 +0530

    Update FileHandler.h

--- include/FileHandler.h
+++ include/FileHandler.h
@@ -1,1 +1,21 @@
+#ifndef FILEHANDLER_H
+#define FILEHANDLER_H
 
+#include <vector>
+#include "Student.h"
+
+class FileHandler {
+private:
+    static const std::string FILENAME;
+    
+public:
+    static bool saveStudent(const Student& student);
+    static std::vector<Student> loadAllStudents();
+    static bool updateStudent(const Student& student);
+    static bool deleteStudent(int rollNo);
+    static bool studentExists(int rollNo);
+    static int getNextRollNo();
+    static void backupData();
+};
+
+#endif
